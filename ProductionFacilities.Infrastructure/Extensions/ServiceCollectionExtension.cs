cfae6b500e130858201fe6113f2c18a0655acb1c
Wajkt0r <wiktor.toczylowski2@gmail.com>
2025-01-20 22:43:05 +0100

    Register dbContext and repositories

---
+++ ProductionFacilities.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductionFacilities.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProductionFacilities.Domain.Contracts.Repositories;
+using ProductionFacilities.Infrastructure.Repositories;
+
+namespace ProductionFacilities.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("ProductionFacilities");
+            services.AddDbContext<ProductionFacilitiesDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped<IContractRepository, ContractRepository>();
+            services.AddScoped<IProductionFacilitiesRepository, ProductionFacilitiesRepository>();
+            services.AddScoped<IEquipmentTypeRepository, EquipmentTypeRepository>();
+        }
+    }
+}
