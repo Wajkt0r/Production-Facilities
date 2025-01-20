6e17385db1ae00a109005037628033fad6e0cdac
Wajkt0r <wiktor.toczylowski2@gmail.com>
2025-01-20 22:45:06 +0100

    Add CommandResult and Mapping profile

---
+++ ProductionFacilities.Application/Common/CommandResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionFacilities.Application.Common
+{
+    public record CommandResult(bool IsSuccess, string Message, int StatusCode)
+    {
+        public static CommandResult Success(string message = "Success", int statusCode = 200)
+            => new CommandResult(true, message, statusCode);
+
+        public static CommandResult Failure(string message, int statusCode = 400)
+            => new CommandResult(false, message, statusCode);
+    }
+}
---
+++ ProductionFacilities.Application/Mapping/ProductionFacilitiesMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ProductionFacilities.Application.DTOs;
+using ProductionFacilities.Domain.Entities;
+
+namespace ProductionFacilities.Application.Mapping
+{
+    public class ProductionFacilitiesMappingProfile : Profile
+    {
+        public ProductionFacilitiesMappingProfile()
+        {
+            CreateMap<Domain.Entities.Contract, ContractDto>()
+                .ReverseMap();
+
+        }
+    }
+}
